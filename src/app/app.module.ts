3025068a62005d6c7c76843eff96438353db8bd8
HadyMohamedMorsy <hady912012@gmail.com>
2023-01-23 18:00:12 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ButtonModule } from 'primeng/button';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import {MenubarModule} from 'primeng/menubar';
+import { HomeComponent } from './pages/home/home.component';
+import { MoviesComponent } from './pages/movies/movies.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, HomeComponent, MoviesComponent],
+  imports: [BrowserModule, AppRoutingModule, ButtonModule, MenubarModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/shared/header/header.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  items!: MenuItem[];
+
+  ngOnInit() {
+    this.items = [
+      {
+        label: 'Home',
+        routerLink: ['/']
+      },
+      {
+        label: 'Movies',
+        routerLink: ['./movies']
+      },
+      {
+        label: 'Tv Shows',
+        routerLink: ['./tvShows']
+      },
+      {
+        label: 'Genres',
+        routerLink: ['./genres']
+      }
+    ];
+  }
+}
