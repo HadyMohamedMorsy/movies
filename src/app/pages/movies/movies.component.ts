31ec72f4dc06727b09f0613c76138d1e1b1f5eec
HadyMohamedMorsy <hady912012@gmail.com>
2023-01-25 18:08:38 +0200

    secound

--- src/app/pages/movies/movies.component.ts
+++ src/app/pages/movies/movies.component.ts
@@ -1,19 +1,25 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { MoviesService } from '../../services/movies.service';
 import { GetMovies, Results } from '../../interface/get-movies';
+import { PrimeNGConfig } from 'primeng/api';
+
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
   styleUrls: ['./movies.component.scss']
 })
-export class MoviesComponent {
+export class MoviesComponent implements OnInit {
+
+  constructor(private primengConfig: PrimeNGConfig) {}
+  
   private slider = inject(MoviesService);
   Popular!: Results[];
-
+  
   ngOnInit(): void {
     this.slider.getMovies('movie/popular', 12).subscribe((data) => {
       this.Popular = data;
     });
-
+    
+    this.primengConfig.ripple = true;
   }
 }
